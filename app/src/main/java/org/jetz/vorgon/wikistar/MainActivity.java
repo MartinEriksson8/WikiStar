041937a87e743dc0be182ef25edb58eb8392744b
Martin-Desktop <martineriksson8@gmail.com>
2018-12-12 15:27:35 +0100

    initial commit after adding some UI elements

---
+++ app/src/main/java/org/jetz/vorgon/wikistar/MainActivity.java
@@ -0,0 +1,30 @@
+package org.jetz.vorgon.wikistar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                opendisplayFeaturedActivity();
+            }
+
+        });
+    }
+  public void opendisplayFeaturedActivity(){
+        Intent OpenPage = new Intent(this, displayFeaturedActivity.class);
+        startActivity(OpenPage);
+  }
+}
---
+++ app/src/main/java/org/jetz/vorgon/wikistar/displayFeaturedActivity.java
@@ -0,0 +1,13 @@
+package org.jetz.vorgon.wikistar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class displayFeaturedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_featured);
+    }
+}
