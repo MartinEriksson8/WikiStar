1efef9c03d4c65b1f371277cd65f8c59b7f3a9c9
Martin-Desktop <martineriksson8@gmail.com>
2018-12-15 15:13:31 +0100

    Created webview and functionality to show Featured content.

--- app/src/main/java/org/jetz/vorgon/wikistar/displayFeaturedActivity.java
+++ app/src/main/java/org/jetz/vorgon/wikistar/displayFeaturedActivity.java
@@ -2,12 +2,20 @@ package org.jetz.vorgon.wikistar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class displayFeaturedActivity extends AppCompatActivity {
+
+    private WebView featured_wiki;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_featured);
+
+        featured_wiki = (WebView) findViewById(R.id.webview_wiki);
+        featured_wiki.setWebViewClient(new WebViewClient());
+        featured_wiki.loadUrl("https://en.wikipedia.org/wiki/Special:RandomInCategory/Featured_articles");
     }
 }
